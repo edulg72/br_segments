14779e8a132230d2c2be085fa03e09a8f0b8ed33
Eduardo Leão Garcia <edulg72@gmail.com>
2016-01-10 11:19:08 -0200

    Fixed breadcrumbs path.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,7 +8,7 @@ class MainController < ApplicationController
   def segments
     @area = CityMapraid.find(params['id'])
     @update = Update.find('segments')
-    @nav = [{@area.name => "/segments/#{@area.gid}"},{ t('nav-first-page') => '/'}]
+    @nav = [{@area.name => "/segments/#{@area.gid}"},{@area.area.name => "/segments_area/#{@area.area.id}"},{ t('nav-first-page') => '/'}]
   end
 
   def segments_area
