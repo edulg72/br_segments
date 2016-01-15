50fbcb737270bc4fe040b8dde952baccf6e7199f
Eduardo Leo Garcia <eduardoleao@tre-rs.jus.br>
2016-01-15 11:36:46 -0200

    Fixed model relationship.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -2,7 +2,7 @@ class Segment < ActiveRecord::Base
   self.table_name = 'vw_segments'
   self.primary_key = 'id'
   
-  has_one :editor, foreign_key: :last_edit_by, class_name: 'User'
+  belongs_to :editor, foreign_key: 'last_edit_by', class_name: 'User'
   belongs_to :street
   belongs_to :city, foreign_key: 'city_id',class_name: 'CityMapraid' 
 
