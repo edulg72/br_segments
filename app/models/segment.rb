caf55956649a11f69727c3360e02116f0e4402a3
Eduardo Leo Garcia <eduardoleao@tre-rs.jus.br>
2016-01-15 11:24:20 -0200

    Fixed model issue.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,6 +1,6 @@
 class Segment < ActiveRecord::Base
   self.table_name = 'vw_segments'
-  has_one :editor, foreign_key: :last_edit_by, class_name: :user
+  has_one :editor, foreign_key: :last_edit_by, class_name: 'User'
   belongs_to :street
   belongs_to :city, foreign_key: 'city_id',class_name: 'CityMapraid' 
 
