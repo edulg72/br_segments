a861af1b6a4acf6580f0e13871c01ae1807ff16e
Eduardo Leo Garcia <eduardoleao@tre-rs.jus.br>
2016-04-15 08:58:31 -0300

    Otimizada montagem da página inicial.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -4,7 +4,7 @@ class Segment < ActiveRecord::Base
 
   belongs_to :editor, foreign_key: 'last_edit_by', class_name: 'User'
   belongs_to :street
-  belongs_to :city, foreign_key: 'city_id',class_name: 'CityShape'
+  belongs_to :city, foreign_key: 'city_id',class_name: 'CityShape', -> { includes :state}
 
   scope :drivable, -> {where(roadtype: [1,2,3,4,6,7,8,15,17,20])}
   scope :important, -> {where(roadtype: [3,4,6,7])}
@@ -15,6 +15,7 @@ class Segment < ActiveRecord::Base
   scope :with_speed, -> {where('(not fwddirection or fwdmaxspeed is not null) and (not revdirection or revmaxspeed is not null)')}
   scope :without_speed, -> {where('((fwddirection and fwdmaxspeed is null) or (revdirection and revmaxspeed is null))')}
   scope :no_roundabout, -> {where('roundabout is null or not roundabout')}
+  scope :weird_level, -> {where('level < -3 or level > 3')}
 
   def self.lock_level(max_lock)
     where("lock is null or lock <= ?",max_lock)
