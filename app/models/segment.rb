1b25501473e3c9ebb3e1cae12b396fb26e3a11f2
Eduardo Leo Garcia <eduardoleao@tre-rs.jus.br>
2016-05-10 08:56:40 -0300

    Adicionado painel para identificar segmentos (não rodovias) com velocidades superiores a 70Km/h.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -10,12 +10,13 @@ class Segment < ActiveRecord::Base
   scope :drivable, -> {where(roadtype: [1,2,3,4,6,7,8,15,17,20])}
   scope :important, -> {where(roadtype: [3,2,6,7])}
   scope :roads, -> {where(roadtype: [3,6,7])}
+  scope :has_speed, -> {where(roadtype: [1,2,3,4,6,7,8])}
   scope :disconnected, -> {where('not connected')}
   scope :no_name, -> {where('street_id is null and not validated')}
   scope :without_name, -> {joins('left outer join vw_streets on vw_streets.id = vw_segments.street_id').where('(street_id is null or vw_streets.isempty) and not alt_names')}
   scope :with_speed, -> {where('(not fwddirection or fwdmaxspeed is not null) and (not revdirection or revmaxspeed is not null)')}
   scope :without_speed, -> {where('((fwddirection and fwdmaxspeed is null) or (revdirection and revmaxspeed is null))')}
-  scope :wrong_speed, -> {joins(:street).where("vw_streets.name !~* '^[[:alpha:]]{2,3}-[[:digit:]]{3}.*' and ((fwddirection and fwdmaxspeed > 70) or (revdirection and revmaxspeed > 70))")}
+  scope :wrong_speed, -> {has_speed.joins(:street).where("vw_streets.name !~* '^[[:alpha:]]{2,3}-[[:digit:]]{3}.*' and ((fwddirection and fwdmaxspeed > 70) or (revdirection and revmaxspeed > 70))")}
   scope :no_roundabout, -> {where('roundabout is null or not roundabout')}
   scope :weird_level, -> {where('level < -3 or level > 3')}
   scope :wrong_city, -> {joins([street: :city], :city).where("cities.name <> '' and not (upper(cities.name) = upper(cities_shapes.nm_municip) or upper(cities.name) like '%('||upper(cities_shapes.nm_municip)||')%')")}
@@ -33,6 +34,6 @@ class Segment < ActiveRecord::Base
 #  end
 
   def location
-    "#{((street_id.nil? or street.nil?) ? I18n.t('noname') : (street.isempty? ? I18n.t('no-street') : street.name.to_s) + ', ' + (street.city_id.nil? ? I18n.t('no-city') : (street.city_isempty? ? I18n.t('no-city') : street.city_name.to_s) + ', ' + ((street.state_id.nil? or street.state_name.empty?) ? I18n.t('no-state') : street.state_name.to_s)))}"
+    "#{((street_id.nil? or street.nil?) ? I18n.t('noname') : (street.isempty? ? I18n.t('no-street') : street.name.to_s) + ', ' + (street.city_id.nil? ? I18n.t('no-city') : (street.city_isempty ? I18n.t('no-city') : street.city_name.to_s) + ', ' + ((street.state_id.nil? or street.state_name.empty?) ? I18n.t('no-state') : street.state_name.to_s)))}"
   end
 end
