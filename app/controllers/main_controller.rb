5e3af9fa65cee59d86f408e806847cb384d3e10d
Eduardo Leão Garcia <edulg72@gmail.com>
2016-10-10 00:12:28 -0300

    Corrigido problema ao buscar usuário.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -26,7 +26,7 @@ class MainController < ApplicationController
   end
 
   def places
-    @user = User.find_by(username: params['id']).first
+    @user = User.find_by(username: params['id'])
     @update = Update.find('places')
     @places = Place.where('created_by = ? or updated_by = ?',@user.id, @user.id)
   end
