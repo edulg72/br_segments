35b4035272345bfae8c6ef6ea5769cc44bd43bd8
Eduardo Leão Garcia <edulg72@gmail.com>
2016-10-09 23:33:24 -0300

    Corrigido modelo de dados dos places.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,8 @@
 class Place < ActiveRecord::Base
+  self.table_name = 'vw_places'
+  self.primary_key = 'id'
+
+  belongs_to :creator, foreign_key: 'created_by', class_name: 'User'
+  belongs_to :editor, foreign_key: 'updated_by', class_name: 'User'
+  belongs_to :city, foreign_key: 'city_id', class_name: 'CityShape'
 end
