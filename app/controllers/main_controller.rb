7442951cb89124a40c6f9552b6068e50be88cc8d
Eduardo Leão Garcia <edulg72@gmail.com>
2016-12-08 15:20:16 -0200

    Criada página para mostrar os editores brasileiros.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -36,7 +36,25 @@ class MainController < ApplicationController
   def places
     @user = User.find_by(username: params['id'])
     @update = Update.find('places')
-    @places = Place.where('created_by = ? or updated_by = ?',@user.id, @user.id)
-    @nav = [{ t('nav-first-page') => '/'}]
+    if @user = nil
+      @places = nil
+    else
+      @places = Place.where('created_by = ? or updated_by = ?',@user.id, @user.id)
+    end
+    @nav = [{'Places' => '#'},{ t('nav-first-page') => '/'}]
+  end
+
+  def editors
+    @editors = User.find_by_sql('select users.id, users.username, users.rank, e.last_edit from users, (select distinct last_edit_by as id, max(last_edit_on) as last_edit from segments where city_id is not null group by last_edit_by) as e where e.id = users.id and e.id > 0 and char_length(trim(both from users.username)) > 1 order by users.username')
+    @update = Update.maximum(:updated_at)
+    @nav = [{'Lista de editores' => '#'},{ t('nav-first-page') => '/'}]
+  end
+
+  def editors_list
+    @editors = User.find_by_sql('select users.id, users.username, users.rank, e.last_edit from users, (select distinct last_edit_by as id, max(last_edit_on) as last_edit from segments where city_id is not null group by last_edit_by) as e where e.id = users.id and e.id > 0 and char_length(trim(both from users.username)) > 1 order by users.username')
+    @update = Update.maximum(:updated_at)
+    @nav = [{'Lista de editores' => '#'},{ t('nav-first-page') => '/'}]
+
+    render layout: false
   end
 end
